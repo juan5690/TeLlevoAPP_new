4b164d4beb221604a840bc0ee364da58f2933554
juan5690 <ju.marinan@duocuc.cl>
2023-11-29 04:35:38 -0300

    agregado cambio de password y 404

--- src/app/recuperar-pass/recuperar-pass.page.ts
+++ src/app/recuperar-pass/recuperar-pass.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-recuperar-pass',
   templateUrl: './recuperar-pass.page.html',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecuperarPassPage implements OnInit {
 
-  constructor() { }
+  FormNuevaPass: FormGroup;
+
+  constructor(public fb: FormBuilder, public alertController: AlertController) { 
+    this.FormNuevaPass = this.fb.group({
+      'nombre': new FormControl("", Validators.required),
+      'password': new FormControl("", Validators.required),
+      'nuevaPass': new FormControl("", Validators.required),
+    });
+  }
 
   ngOnInit() {
   }
 
+  async Modificar() {
+    var f = this.FormNuevaPass.value;
+
+    var newPass = {
+      password: f.nuevaPass,
+    }
+    
+    localStorage.setItem('password', JSON.stringify(newPass));
+
+    // Llamamos a la función presentAlert para mostrar la alerta con un mensaje personalizado
+    this.presentAlert('Contraseña modificada con éxito!');
+  }
+
+  async presentAlert(message: string) {
+    const alert = await this.alertController.create({
+      header: 'Mensaje',
+      message: message,
+      buttons: ['Aceptar']
+    });
+
+    await alert.present();
+  }
 }
