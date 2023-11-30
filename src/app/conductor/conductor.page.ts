70b2f75566dd0db47b6183c70ec292d08e7a0b54
juan5690 <ju.marinan@duocuc.cl>
2023-11-30 02:19:19 -0300

    descargadas imagenes para la aoi y conductor shit

--- src/app/conductor/conductor.page.ts
+++ src/app/conductor/conductor.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-conductor',
@@ -7,9 +9,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConductorPage implements OnInit {
 
-  constructor() { }
+  formularioConductor: FormGroup;
+
+  constructor(
+    public fb: FormBuilder,
+    public alertController: AlertController,
+    public navCTRL: NavController
+  ) { 
+    this.formularioConductor = this.fb.group({
+      'nombre': new FormControl("", Validators.required),
+      'vehiculo': new FormControl("", Validators.required),
+      'capacidad': new FormControl("", Validators.required),
+      'precio': new FormControl("", [Validators.required, Validators.pattern(/^\d+(\.\d{1,2})?$/)]),
+      'destino': new FormControl("", Validators.required),
+    });
+  }
 
   ngOnInit() {
   }
 
+  async registrarConductor() {
+    var f = this.formularioConductor.value;
+
+    if (this.formularioConductor.invalid) {
+      const alert = await this.alertController.create({
+        header: 'Error al registrar',
+        message: 'Debes completar todos los campos correctamente!',
+        buttons: ['Aceptar'],
+      });
+  
+      await alert.present();
+      return;
+    }
+
+    var conductor = {
+      nombreConductor: f.nombre,
+    }
+
+    var modAuto = {
+      auto: f.vehiculo,
+    }
+
+    var destino = {
+      destino: f.destino,
+    }
+
+    var precio = {
+      precio: f.precio,
+    }
+
+    localStorage.setItem('nombreConductor', JSON.stringify(conductor));
+    localStorage.setItem('modAuto', JSON.stringify(modAuto));
+    localStorage.setItem('destino', JSON.stringify(destino));
+    localStorage.setItem('precio', JSON.stringify(precio));
+
+    this.navCTRL.navigateRoot(['/menu', { nombre: f.nombre }]);
+  }
 }
+
